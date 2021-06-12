407e4a42b591bc167a46b92e43e13dbb0a7e0427
Tomas Krulis <krulis.tomas.tk@gmail.com>
2021-06-12 20:26:31 +0200

    Added loading of python code to C. Working.

--- DirectInterop/src/C/libpythonhello.c
+++ DirectInterop/src/C/libpythonhello.c
@@ -5,9 +5,15 @@ int hello_from_python(int argc, char ** argv) {
 
     char * code = "print('Hello from Python! As simple function.')";
 
+	char * filename = "scripts/helloPython.py";
+	FILE* fp;
+
 	Py_Initialize();
 
 	PyRun_SimpleString(code);
+
+	fp = _Py_fopen(filename, "r");
+	PyRun_SimpleFile(fp, filename);
 
 	Py_Finalize();
 
